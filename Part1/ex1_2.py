6b9d72214362ff6026ab3369e2cfe2b871fe8d4f
Manuel <gomesnelito@gmail.com>
2021-10-14 14:06:11 +0100

    A new script, talking to your friends

---
+++ Part1/ex1_2.py
@@ -0,0 +1,7 @@
+def main():
+    names = ['Alváro de Campos', 'Alberto Caeiro', 'Bernardo Soares', 'Fernando Pessoa', 'Ricardo Reis']
+    for name in names:
+        print('Hello ' + name + '!')
+
+if __name__ == "__main__":
+    main()
