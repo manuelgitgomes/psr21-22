20e093ae522c969b6fd51b7d202413f070adb4a8
Manuel <gomesnelito@gmail.com>
2021-11-11 14:41:25 +0000

    Easy opencv algorithm done

---
+++ Part6/Ex1/main1ab.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import copy
+from functools import partial
+import numpy as np
+import cv2
+
+
+def main():
+    # Creating image
+    img = cv2.imread('/home/nel/catkin_ws/src/psr21-22/Part5/imgs/atlascar.png')
+
+    # Window name definition
+    window_name = 'window - Ex1c'
+
+    # Creating window
+    cv2.namedWindow(window_name)
+
+    # Image shape
+    h, w, _ = img.shape
+
+    # Drawing a circle and placing test
+    cv2.circle(img, (int(w/2), int(h/2)), 200, (255, 0, 0), 5)
+    cv2.putText(img, 'PSR', (int(w/2), int(h/2)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
+
+    # Show image
+    cv2.imshow(window_name, img)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
